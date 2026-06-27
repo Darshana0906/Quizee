9c3591557b4d003f09ffea8a3b76990e45d4c560
Darshana0906 <mothersdarshana@gmail.com>
2026-06-27 14:33:42 +0530

    Added seesion validation after login

--- student/dashboard.php
+++ student/dashboard.php
@@ -1,3 +1,11 @@
 <?php
+session_start();
+
+if(!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'student'){
+    header("Location: login.php");
+    exit;
+}
+?>
+<?php
 echo "Welcome to the student dashboard!";
 ?>
